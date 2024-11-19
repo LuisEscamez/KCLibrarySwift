e288a6438cdaea97ea725a743778ac757e4d48fb
LuisEscamez <muligor@hotmail.com>
2024-11-19 19:46:53 +0100

    Keychain changes

---
+++ Sources/KCLibrarySwift/KeyChain.swift
@@ -0,0 +1,29 @@
+import Foundation
+import Security
+import KeychainSwift
+
+public struct KeyChainKC {
+    
+    @discardableResult public func saveKC(key: String, value: String) -> Bool {
+        if let data = value.data(using: .utf8) {
+            let keychain = KeychainSwift()
+            return keychain.set(data, forKey: key)
+        } else {
+            return false
+        }
+    }
+    
+    public func loadKC(key: String) -> String? {
+        let keychain = KeychainSwift()
+        if let data = keychain.get(key) {
+            return data
+        } else {
+            return ""
+        }
+    }
+    
+    @discardableResult public func deleteKC(key: String) -> Bool {
+        let keychain = KeychainSwift()
+        return keychain.delete(key)
+    }
+}
